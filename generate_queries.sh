ef6ea235c728969d32e033a66cd0ee1a577d17e4
Milos Nikolic <milos.nikolic83@gmail.com>
2018-12-20 23:41:24 +0000

    removing unused code

--- generate_queries.sh
+++ generate_queries.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-# sbt "
-# ;run examples/sql/sum/retailer_ALL.sql examples/dtree/retailer.txt -o experiments/src/queries/sum/retailer/Sum_Retailer_DF-INCR_ALL_GENERATED.m3
-# ;run examples/sql/sum/retailer_INVENTORY.sql examples/dtree/retailer.txt -o experiments/src/queries/sum/retailer/Sum_Retailer_DF-INCR_INVENTORY_GENERATED.m3
-# ;run examples/sql/sum/housing_ALL.sql examples/dtree/housing.txt -o experiments/src/queries/sum/housing/Sum_Housing_DF-INCR_ALL_GENERATED.m3
-# ;run examples/sql/cofactor/retailer_ALL.sql examples/dtree/retailer.txt -o experiments/src/queries/cofactor/retailer/Cofactor_Retailer_DF-INCR_ALL_GENERATED.m3
-# ;run examples/sql/cofactor/retailer_INVENTORY.sql examples/dtree/retailer.txt -o experiments/src/queries/cofactor/retailer/Cofactor_Retailer_DF-INCR_INVENTORY_GENERATED.m3
-# ;run examples/sql/cofactor/housing_ALL.sql examples/dtree/housing.txt -o experiments/src/queries/cofactor/housing/Cofactor_Housing_DF-INCR_ALL_GENERATED.m3
-# ;run examples/sql/relational/retailer_ALL.sql examples/dtree/retailer.txt -o experiments/src/queries/full_join/retailer/Relational_Retailer_DF-INCR_ALL_GENERATED.m3
-# ;run examples/sql/relational/retailer_INVENTORY.sql examples/dtree/retailer.txt -o experiments/src/queries/full_join/retailer/Relational_Retailer_DF-INCR_INVENTORY_GENERATED.m3
-# ;run examples/sql/relational/housing_ALL.sql examples/dtree/housing.txt -o experiments/src/queries/full_join/housing/Relational_Housing_DF-INCR_ALL_GENERATED.m3
-# ;run examples/sql/factorized/retailer_ALL.sql examples/dtree/retailer.txt -o experiments/src/queries/full_join/retailer/Factorized_Retailer_DF-INCR_ALL_GENERATED.m3
-# ;run examples/sql/factorized/retailer_INVENTORY.sql examples/dtree/retailer.txt -o experiments/src/queries/full_join/retailer/Factorized_Retailer_DF-INCR_INVENTORY_GENERATED.m3
-# ;run examples/sql/factorized/housing_ALL.sql examples/dtree/housing.txt -o experiments/src/queries/full_join/housing/Factorized_Housing_DF-INCR_ALL_GENERATED.m3
-# "
-
 sbt "
 ; run examples/sql/factorized/housing_ALL.sql examples/dtree/housing.txt -o experiments/src/queries/full_join/housing/Factorized_Housing_DF-INCR_ALL_GENERATED.m3
 ; run examples/sql/factorized/retailer_ALL.sql examples/dtree/retailer.txt -o experiments/src/queries/full_join/retailer/Factorized_Retailer_DF-INCR_ALL_GENERATED.m3
