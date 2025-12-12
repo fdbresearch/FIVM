5b350d3a18c18594443aefc0e58873e8bb8d1031
mnikolic <milos.nikolic@ed.ac.uk>
2025-12-12 00:27:08 +0000

    Removed backend folder

--- backend/dbtoaster/examples/queries/simple/r_sumadivsumb.sql
+++
@@ -1,8 +0,0 @@
-CREATE STREAM R(A float, B float) 
-  FROM FILE 'examples/data/simple/r.dat' LINE DELIMITED
-  CSV ();
-
-/* We insert a LISTMAX to support incremental computation. */
-
-SELECT SUM(A)/LISTMAX(1, 1+SUM(B)) FROM R
-
